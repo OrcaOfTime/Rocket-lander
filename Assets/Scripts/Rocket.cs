41f06feec0d3ae9431c12ea21e88e2162db373d9
OrcaOfTime <mwilliams.sean@gmail.com>
2020-10-18 17:24:23 +0100

    Move and sound
    Basic movement and rotation on shop
    Rocket sound plays on thrust

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+
+    Rigidbody rocketBody;
+    AudioSource shipaudio;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rocketBody = GetComponent<Rigidbody>();
+        shipaudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -16,12 +21,29 @@ public class Rocket : MonoBehaviour
         ProcessInput();
     }
 
-    private void ProcessInput() { 
-        
-        if(Input.GetKey(KeyCode.Space))
-        {
+    private void ProcessInput() {
 
+        //Rocket boosts forward, can boost whilst rotating
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rocketBody.AddRelativeForce(Vector3.up);
+            if (!shipaudio.isPlaying) shipaudio.Play();
         }
+        else shipaudio.Stop();
+
+        //No rotation if both keys are being pressed at the same time 
+        if(!(Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)))
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(Vector3.forward * 0.5f);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(Vector3.back * 0.5f);
+            }
+        }
+        
     }
 
 
